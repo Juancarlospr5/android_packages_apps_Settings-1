ca05eceeb733b697ab0f5d7a8e8b155889ac7033
Clark Scheff <clark@scheffsblend.com>
2017-09-22 09:16:51 +0800

    Guard against themes without colorAccent defined
    If an applied theme omits the colorAccent attribute in their style
    AppOpsSummary will crash because typedValue.resourceId is 0.
    
    Check if typedValue.resourceId is 0 and if so use switch_accent_color
    for tab indicator color.
    
    Change-Id: Idc1ac94053d46515ec74797a7a149cbc4d0ce98b

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -25,6 +25,7 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFrameLayout;
@@ -53,7 +54,7 @@ import com.android.settings.R;
 public class AppOpsSummary extends InstrumentedPreferenceFragment {
     // layout inflater object used to inflate views
     private LayoutInflater mInflater;
-    
+
     private ViewGroup mContentContainer;
     private View mRootView;
     private ViewPager mViewPager;
@@ -161,7 +162,9 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         Resources.Theme theme = tabs.getContext().getTheme();
         TypedValue typedValue = new TypedValue();
         theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true);
-        final int colorAccent = getContext().getColor(typedValue.resourceId);
+        final int colorAccent = typedValue.resourceId != 0
+                ? getContext().getColor(typedValue.resourceId)
+                : getContext().getColor(R.color.fingerprint_title_area_bg);
         tabs.setTabIndicatorColor(colorAccent);
 
         // We have to do this now because PreferenceFrameLayout looks at it
