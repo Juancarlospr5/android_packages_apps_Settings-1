650b2e6447486539d402fb4d728176ecfe706641
Chris <chris@cypheros.co>
2017-12-31 19:35:07 -0500

    Settings: Introduce Color Manager (ColorOM)
    Color Manager a.k.a Color Overlay Manager (ColorOM) is a light interface
    for applying themes and accents. You can choose between base themes such
    as (Light, Dark or Automatic etc.) or choose between several system color accents.
    
    Automatic: Restores aosp theming logic, by theming the system based
    on the wallpaper that is selected.
    
    Light: Forces the systems light (default) theme and does not change
    regardless if the wallpaper is changed.
    
    Dark: Forces the systems dark theme and does not change regardless
    if the wallpaper is changed.
    
    ColorOM Links the default "Theme" changer from aoscp which allows
    changing system accents via overlay package.
    
    Contributors:
    @maxwen - Dark mode logic
    @sagarshah1729
    @AgentFabulous
    
    Change-Id: If869a36b73ebc5cfd79a31e168b3fd79b7475351

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -21,6 +21,7 @@ import android.provider.SearchIndexableResource;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.aoscp.display.ColorManagerPreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.AutoBrightnessPreferenceController;
@@ -107,6 +108,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         //controllers.add(new ThemePreferenceController(context));
+        controllers.add(new ColorManagerPreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
