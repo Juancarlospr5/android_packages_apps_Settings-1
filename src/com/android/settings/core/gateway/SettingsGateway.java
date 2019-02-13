Havoc-OS/android_packages_apps_Settings

Add Pie control activity

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -151,6 +151,7 @@ import com.havoc.settings.fragments.ScreenStateToggles;
 import com.havoc.settings.fragments.SmartPixels;
 import com.havoc.settings.fragments.Weather;
 import com.havoc.settings.fragments.ui.DozeFragment;
+import com.havoc.settings.fragments.PieSettings;
 
 public class SettingsGateway {
 
@@ -289,7 +290,8 @@ public class SettingsGateway {
             Display.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             ScreenStateToggles.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            PieSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

Juancarlospr5/android_packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -152,6 +152,7 @@ import com.arsenic.settings.fragments.ScreenStateToggles;
 import com.arsenic.settings.fragments.SmartPixels;
 import com.arsenic.settings.fragments.Weather;
 import com.arsenic.settings.fragments.ui.DozeFragment;
+import com.arsenic.settings.fragments.PieSettings;
 
 public class SettingsGateway {
 
@@ -291,7 +292,8 @@ public class SettingsGateway {
             Display.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             ScreenStateToggles.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            PieSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {