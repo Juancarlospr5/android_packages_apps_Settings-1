Havoc-OS/android_packages_apps_Settings

Settings: Adapt for vendor_support

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -147,12 +147,12 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.havoc.settings.HavocSettings;
 import com.havoc.settings.fragments.Buttons;
 import com.havoc.settings.fragments.Display;
+import com.havoc.settings.fragments.DozeFragment;
 import com.havoc.settings.fragments.ExpandedDesktopPreferenceFragment;
 import com.havoc.settings.fragments.HeadsUpSettings;
 import com.havoc.settings.fragments.Interface;
-import com.havoc.settings.fragments.ScreenStateToggles;
-import com.havoc.settings.fragments.ui.DozeFragment;
 import com.havoc.settings.fragments.PieSettings;
+import com.havoc.settings.fragments.ScreenStateToggles;
 
 public class SettingsGateway {
 

src/com/android/settings/fuelgauge/BatteryChargeSoundSettings.java
@@ -33,7 +33,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
+import com.havoc.support.preferences.CustomSeekBarPreference;
 
 public class BatteryChargeSoundSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -44,7 +44,7 @@ public class BatteryChargeSoundSettings extends SettingsPreferenceFragment imple
     private static final String KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR = "battery_level_charge_seek_bar";
     private static final String KEY_SOUND_SILENT = "silent";
 
-    private SystemSettingSeekBarPreference mBatteryLevelChargeSeekbar;
+    private CustomSeekBarPreference mBatteryLevelChargeSeekbar;
     private Preference mBatteryLevelChargeRingtone;
     private SwitchPreference mBatteryLevelChargeAlarmEnabled;
 
@@ -58,7 +58,7 @@ public class BatteryChargeSoundSettings extends SettingsPreferenceFragment imple
                 Settings.System.BATTERY_LEVEL_CHARGE_ALARM_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
         mBatteryLevelChargeAlarmEnabled.setOnPreferenceChangeListener(this);
 
-        mBatteryLevelChargeSeekbar = (SystemSettingSeekBarPreference) findPreference(KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR);
+        mBatteryLevelChargeSeekbar = (CustomSeekBarPreference) findPreference(KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR);
         int batterylevelchargeseekbar = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND, 100, UserHandle.USER_CURRENT);
         mBatteryLevelChargeSeekbar.setValue(batterylevelchargeseekbar);

src/com/android/settings/notification/CustomLightOffTimePreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
+import com.havoc.support.preferences.CustomSeekBarPreference;
 
 public class CustomLightOffTimePreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
@@ -61,7 +61,7 @@ public class CustomLightOffTimePreferenceController extends NotificationPreferen
     public void updateState(Preference preference) {
         if (mChannel != null) {
             //light off time pref
-            SystemSettingSeekBarPreference mLightOffTime = (SystemSettingSeekBarPreference) preference;
+            CustomSeekBarPreference mLightOffTime = (CustomSeekBarPreference) preference;
             int lightOff = mChannel.getLightOffTime();
             int defaultLightOff = mContext.getResources().getInteger(
                     com.android.internal.R.integer.config_defaultNotificationLedOff);

src/com/android/settings/notification/CustomLightOnTimePreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
+import com.havoc.support.preferences.CustomSeekBarPreference;
 
 public class CustomLightOnTimePreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
@@ -61,7 +61,7 @@ public class CustomLightOnTimePreferenceController extends NotificationPreferenc
     public void updateState(Preference preference) {
         if (mChannel != null) {
             //light on time pref
-            SystemSettingSeekBarPreference mLightOnTime = (SystemSettingSeekBarPreference) preference;
+            CustomSeekBarPreference mLightOnTime = (CustomSeekBarPreference) preference;
             int lightOn = mChannel.getLightOnTime();
             int defaultLightOn = mContext.getResources().getInteger(
                     com.android.internal.R.integer.config_defaultNotificationLedOn);

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.havoc.support.colorpicker.ColorPickerPreference;
 
 public class CustomLightsPreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {

Juancarlospr5/android_packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,16 +144,15 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
-<<<<<<< HEAD
 import com.arsenic.settings.ArsenicSettings;
 import com.arsenic.settings.fragments.HeadsUpSettings;
 import com.arsenic.settings.fragments.Buttons;
 import com.arsenic.settings.fragments.Display;
+import com.arsenic.settings.fragments.DozeFragment;
 import com.arsenic.settings.fragments.ExpandedDesktopPreferenceFragment;
 import com.arsenic.settings.fragments.Interface;
-import com.arsenic.settings.fragments.ScreenStateToggles;
-import com.arsenic.settings.fragments.ui.DozeFragment;
 import com.arsenic.settings.fragments.PieSettings;
+import com.arsenic.settings.fragments.ScreenStateToggles;
 
 public class SettingsGateway {
 

src/com/android/settings/fuelgauge/BatteryChargeSoundSettings.java
@@ -33,7 +33,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
+import com.arsenic.support.preferences.CustomSeekBarPreference;
 
 public class BatteryChargeSoundSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -44,7 +44,7 @@ public class BatteryChargeSoundSettings extends SettingsPreferenceFragment imple
     private static final String KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR = "battery_level_charge_seek_bar";
     private static final String KEY_SOUND_SILENT = "silent";
 
-    private SystemSettingSeekBarPreference mBatteryLevelChargeSeekbar;
+    private CustomSeekBarPreference mBatteryLevelChargeSeekbar;
     private Preference mBatteryLevelChargeRingtone;
     private SwitchPreference mBatteryLevelChargeAlarmEnabled;
 
@@ -58,7 +58,7 @@ public class BatteryChargeSoundSettings extends SettingsPreferenceFragment imple
                 Settings.System.BATTERY_LEVEL_CHARGE_ALARM_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
         mBatteryLevelChargeAlarmEnabled.setOnPreferenceChangeListener(this);
 
-        mBatteryLevelChargeSeekbar = (SystemSettingSeekBarPreference) findPreference(KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR);
+        mBatteryLevelChargeSeekbar = (CustomSeekBarPreference) findPreference(KEY_BATTERY_LEVEL_CHARGE_SEEK_BAR);
         int batterylevelchargeseekbar = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND, 100, UserHandle.USER_CURRENT);
         mBatteryLevelChargeSeekbar.setValue(batterylevelchargeseekbar);

src/com/android/settings/notification/CustomLightOffTimePreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
+import com.arsenic.support.preferences.CustomSeekBarPreference;
 
 public class CustomLightOffTimePreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
@@ -61,7 +61,7 @@ public class CustomLightOffTimePreferenceController extends NotificationPreferen
     public void updateState(Preference preference) {
         if (mChannel != null) {
             //light off time pref
-            SystemSettingSeekBarPreference mLightOffTime = (SystemSettingSeekBarPreference) preference;
+            CustomSeekBarPreference mLightOffTime = (CustomSeekBarPreference) preference;
             int lightOff = mChannel.getLightOffTime();
             int defaultLightOff = mContext.getResources().getInteger(
                     com.android.internal.R.integer.config_defaultNotificationLedOff);

src/com/android/settings/notification/CustomLightOnTimePreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
+import com.arsenic.support.preferences.CustomSeekBarPreference;
 
 public class CustomLightOnTimePreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
@@ -61,7 +61,7 @@ public class CustomLightOnTimePreferenceController extends NotificationPreferenc
     public void updateState(Preference preference) {
         if (mChannel != null) {
             //light on time pref
-            SystemSettingSeekBarPreference mLightOnTime = (SystemSettingSeekBarPreference) preference;
+            CustomSeekBarPreference mLightOnTime = (CustomSeekBarPreference) preference;
             int lightOn = mChannel.getLightOnTime();
             int defaultLightOn = mContext.getResources().getInteger(
                     com.android.internal.R.integer.config_defaultNotificationLedOn);

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.arsenic.support.colorpicker.ColorPickerPreference;
 
 public class CustomLightsPreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {